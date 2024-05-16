ba9eb64d77788a4a0e16dff2ee8fb21938f30ba0
leam69 <105415381+leam69@users.noreply.github.com>
2024-05-16 09:37:40 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-// Include auth.php to get $name variable
+
 include 'auth.php';
 ?>
 
@@ -8,14 +8,14 @@ include 'auth.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Docker Project</title>
+  <title>AWS Project</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><?php echo $SESSION['username'] ?></a>
+    <a class="navbar-brand" href="#"><?php echo $_SESSION['username'] ?></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
